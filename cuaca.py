d7393955a2713039f29ade1b779fa15042493dac
sholikun32 <102804771+sholikun32@users.noreply.github.com>
2023-09-14 22:13:23 +0700

    Create cuaca.py

---
+++ cuaca.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+
+# Judul aplikasi
+st.title("Aplikasi Prakiraan Cuaca Kota Semarang")
+
+# Koordinat lokasi Semarang
+latitude = -7.005145  # Ganti dengan koordinat latitude yang sesuai
+longitude = 110.438125  # Ganti dengan koordinat longitude yang sesuai
+
+# API Key
+api_key = "rzNAAHgYeddgMbZf5nWaTC6gnn4q2rM3"
+
+# URL API
+url = f"https://api.tomorrow.io/v4/weather/forecast?location={latitude},{longitude}&apikey={api_key}"
+
+# Mengambil data cuaca dari API
+response = requests.get(url)
+
+# Memeriksa apakah permintaan berhasil atau tidak
+if response.status_code == 200:
+    data = response.json()
+
+    # Menampilkan informasi cuaca
+    st.subheader("Informasi Cuaca")
+    st.write(f"Lokasi: {data['location']['name']}, {data['location']['country']}")
+
+    # Menampilkan prakiraan cuaca
+    st.subheader("Prakiraan Cuaca")
+    for forecast in data['data']['timelines'][0]['intervals']:
+        time = forecast['startTime']
+        temperature = forecast['values']['temperature']
+        precipitation = forecast['values']['precipitationIntensity']
+        st.write(f"Waktu: {time}, Temperatur: {temperature}°C, Intensitas Presipitasi: {precipitation} mm/h")
+
+else:
+    st.error("Gagal mengambil data cuaca. Silakan coba lagi nanti.")
